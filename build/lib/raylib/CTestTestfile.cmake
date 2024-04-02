00f3f591bb4a23d890df741b4ad7e8ee1a1a7046
jusoue <67708393+jusoue@users.noreply.github.com>
2024-04-02 00:27:12 -0400

    Delete build directory

--- build/lib/raylib/CTestTestfile.cmake
+++
@@ -1,7 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: C:/dev/space_invaders/lib/raylib
-# Build directory: C:/dev/space_invaders/build/lib/raylib
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
-subdirs("raylib")
